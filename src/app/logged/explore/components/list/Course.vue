cff2707db1248986239cb48c80655bcc56d739d7
Ussuy <artussuy7@gmail.com>
2020-05-01 18:22:55 -0300

    course and list components added

---
+++ src/app/logged/explore/components/list/Course.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="c-course">
+    <p class="c-course_name">{{ course.name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+  props: {
+    course: {required: true}
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/css/mixins/text.scss';
+.c-course {
+  background-color: #E6E6F0;
+  border-radius: 15%;
+  padding: 6px;
+  width: 120px;
+  height: 120px;
+  display: flex;
+  align-items: center;
+  &_name {
+    @include make-course-name();
+    color: black;
+  }
+}
+</style>
---
+++ src/app/logged/explore/components/list/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="c-list">
+      <Course v-for="(course, index) in courses" :key="index" :course="course" />
+  </div>
+</template>
+
+<script>
+import Course from './Course'
+
+export default {
+
+  components: {
+    Course
+  },
+
+  data() {
+    return {
+      courses: [
+        {
+          name: 'PHP for dummies'
+        },
+        {
+          name: 'HTML5'
+        },
+        {
+          name: 'Python Tutorial for Beginners'
+        },
+      ]
+    } 
+  }
+}
+</script>
+
+<style lang="scss">
+.c-list {
+  display: grid;
+  grid-template: auto / 1fr 1fr;
+  grid-gap: 15px;
+  justify-items: center;
+  padding-top: 25px;
+}
+</style>
